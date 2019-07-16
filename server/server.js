9b3e3650c067f6fe87e911dcdc1456fc17817552
Godfrey Martinez <godmartinz@gmail.com>
2019-07-16 14:25:00 -0700

    validates values server side

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const routes = require("./routes");
-const  validate = require('express-validation');
+const {check, validationResult}  = require('express-validator');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.static("public"));
 app.use(morgan("dev"));
 app.use('/routes', routes);
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
@@ -22,18 +22,39 @@ app.get("/project/submit/:projectname", (req, res) => {
   const food = { candy: 'candy' };
   res.render('submit', food);
 });
-
-app.post("/project/authenticate/:projectname", (req, res) => {
-  const mockObject = {
-    bob: "alsoBob"
-  };
-  res.status(200).json(mockObject);
+/* Validates all required keys are strings and are populated */
+app.post("/project/authenticate/:projectname", 
+ [ check('lti_message_type').isString().not().isEmpty(),
+    check('lti_version').isString().not().isEmpty(),
+    check('Oauth_consumer_key').isString().not().isEmpty(),
+    check('resource_link_id').isString().not().isEmpty(),
+    check('client_id').isString().not().isEmpty(),
+    check('redirect_uri').isString().not().isEmpty(),
+    check('responce_type').isString().not().isEmpty(),
+    check('scope').isString().not().isEmpty(),
+    check('custom_user_role').isString().not().isEmpty(),
+    check('custom_project_name').isString().not().isEmpty()
+],(req, res) => {
+  
+    const errors= validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(422).json({errors: errors.array()});
+    }
+    let userInfo={
+      lti_message_type:req.body.lti_message_type,
+      lti_version: req.body.lti_version,
+      Oauth_consumer_key: req.body.Oauth_consumer_key,
+      resource_link_id: req.body.resource_link_id,
+      client_id: req.body.client_id,
+      redirect_uri: req.body.redirect_uri,
+      responce_type:req.body.responce_type,
+      scope:req.body.scope,
+      custom_user_role:req.body.custom_user_role,
+      custom_project_name:req.body.custom_project_name
+    }
+  res.send(200, userInfo).json(userInfo);
 });
 
-app.post(`/project/authentication/:projectname`, validate(validation.userInfo) , (req,res) => {
-
-  res.json(200);
-});
 
 
 
