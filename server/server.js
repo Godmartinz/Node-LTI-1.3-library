49dbd6e906edd580c15c7d06540e80b6a04bca1c
GianFiyah <gian.delprado94@gmail.com>
2019-07-17 16:35:41 -0700

    Gd grading tool (#10) (#11)
    merges PR #10 into master.

--- server/server.js
+++ server/server.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require('body-parser');
 const routes = require("./routes");
 const {check, validationResult}  = require('express-validator');
-
-
+const axios = require('axios');
 
 const app = express();
 
 app.use(express.static("public"));
 app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/routes', routes);
 app.use(express.json());
+app.use( (req,res,next) => {
+  res.locals.formData = null;
+  next();
+});
 
 
 app.set('views', './views');
@@ -21,14 +27,36 @@ app.get("/", (req, res) => {
 });
 
 app.get("/project/submit/:projectname", (req, res) => {
-  const food = { candy: 'candy' };
-  res.render('submit', food);
-
+  res.render('submit', {food: 'candy'});
 });
+
+app.post(`/project/submit/projectname`, (req, res) => {
+  const url = req.body;
+  if (url.github.includes('.herokuapp.com') || url.github.includes('github.com')) {
+    axios
+    .get(url.github)
+      .then( response => {
+        let grading = {
+          url: url.github,
+          urlStatus: response.status,
+          grade: 1
+        };
+        res.render('submit', {food: 'candy', formData: grading});
+      })
+      .catch( err => console.log(err))
+  } else {
+    let grading = {
+      url: url.github,
+      urlStatus: 'Sorry, the Urls you provided are not valid.',
+      grade: 0
+    };
+    res.render('submit', {food: 'candy', formData: grading});
+  }
+})
 
 /* Validates all required keys are strings and are populated. These may need to be updated moving forward. */
 app.post("/project/authenticate/:projectname", 
- [ check('https://purl.imsglobal.org/spec/lti/claim/message_type').exists().isString().not().isEmpty().withMessage("There is an error in the message type"),
+  [ check('https://purl.imsglobal.org/spec/lti/claim/message_type').exists().isString().not().isEmpty().withMessage("There is an error in the message type"),
     check('https://purl.imsglobal.org/spec/lti/claim/version').exists().isString().not().isEmpty().withMessage("There is an error with the version"),
     check('https://purl.imsglobal.org/spec/lti/claim/deployment_id').exists().isString().not().isEmpty().withMessage("There is an error with the deployment_id"),
     check('https://purl.imsglobal.org/spec/lti/claim/resource_link').exists().withMessage("There is an error with the resource_link"),
@@ -44,7 +72,16 @@ app.post("/project/authenticate/:projectname",
     check('user_role').exists().isString().contains("http://purl.imsglobal.org/vocab/lis/v2/membership#Learner").withMessage("The role of the user is not learner or undefined"),
     check('project_name').exists().isString().not().isEmpty().withMessage("There is an error with the project_name")
 ],(req, res) => {
-    
+
+  switch (req) {
+    case req:
+      const errors= validationResult(req);
+      if(!errors.isEmpty()){
+        return res.status(422).json({errors: errors.array()});
+      } else res.send(200).json(req);
+      break;
+  }
+
     const errors= validationResult(req);
     if(!errors.isEmpty()){
       return res.status(422).json({errors: errors.array()});
@@ -52,7 +89,5 @@ app.post("/project/authenticate/:projectname",
     
   res.send(200).json(req.body);
 });
-
-
 
 module.exports = app;
