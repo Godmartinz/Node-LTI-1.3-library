037f6d48d78bf704771b3c74881338f22fe65bd6
Sherry Freitas <44448953+sfreitas9@users.noreply.github.com>
2019-07-18 10:47:10 -0700

    adds oauth2 client credentials flow (#13)
    * adds basic oauth client certification validation and related test suite (#12)
    
    Make sure you add the .env variables

---
+++ server/oauth2_validation.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+
+/* 
+ * Validate LTI 1.3 OAuth2 Client Credentials Request
+ * @param req - HTTP request to validate
+ * @returns an array, which if empty indicates 0 errors, otherwise, contains description of each error
+*/
+function valid_oauth2_request(req) {
+  let body = req.body;
+  let errors = [];
+
+  // Check it is a POST request
+  if (req.method !== 'POST') {
+    errors.push('Method invalid');
+  };
+
+  // Check the data exists
+  if (Object.entries(body).length === 0 && body.constructor === Object) {
+    errors.push('body data missing');
+  } else {
+    // Check the grant type
+    if (!body.hasOwnProperty('grant_type')) {
+      errors.push('grant type missing')
+    } else {
+      if (body.grant_type !== 'client_credentials') {
+        errors.push('grant type invalid')
+      }
+    }
+
+    // Check the client_id
+    // TODO:  We should have a database of valid client_ids
+    if (!body.hasOwnProperty('client_id')) {
+      errors.push('client id missing')
+    } else {
+      if (body.client_id !== process.env.CLIENT_ID) {
+        errors.push('client id invalid')
+      }
+    }
+
+    // Check the client_secret
+    // TODO:  We need to connect client_id with their secret
+    if (!body.hasOwnProperty('client_secret')) {
+      errors.push('client secret missing')
+    } else {
+      if (body.client_secret !== process.env.CLIENT_SECRET) {
+        errors.push('client secret invalid')
+      }
+    }
+  }
+  
+  return errors;
+}
+
+module.exports = { valid_oauth2_request };
