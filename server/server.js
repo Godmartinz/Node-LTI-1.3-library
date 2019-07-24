3ba5a240d9d1c874485358196c4f80803c8aa650
Godfrey Martinez <godmartinz@gmail.com>
2019-07-24 10:10:44 -0700

    seperated validation into the LTI_Lib

--- server/server.js
+++ server/server.js
@@ -29,9 +29,10 @@ app.get("/", (req, res) => {
 app.post("/oauth2/token", (req, res) => {
   var errors = valid_oauth2_request(req);
   tokenMaker(errors, res);
+  
 });
 
-app.post("/course/module/submit/:project", (req, res) => {
+app.post("/project/submit/:project", (req, res) => {
   keyValidator(req, res);
 });
 
