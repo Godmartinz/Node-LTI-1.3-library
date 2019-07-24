79c66e76f3b36a83b3dbeb4c586c4fbe8e25f454
Sherry Freitas <44448953+sfreitas9@users.noreply.github.com>
2019-07-24 16:21:18 -0700

    adds student name and course information parameter support in library and usage in tool (#25)

--- tests/request_validation.spec.js
+++ tests/request_validation.spec.js
@@ -1,16 +1,18 @@
 const expect = require('chai').expect;
-const { valid_launch_request } = require('../server/launch_validation.js');
+const { valid_launch_request } = require('../lti_lib/launch_validation.js');
 
 describe('Validate Launch Request', () => {
   let req = null;
 
   beforeEach(() => {
-    req = { method: 'POST',
+    req = { 
+      method: 'POST',
       url: 'http://this.is.a.fake.url',
-      headers: 
-      { 'Content-Type': 'application/json' },
-      body: 
-      { sub: 'a6d5c443-1f51-4783-ba1a-7686ffe3b54a',
+      headers: {
+        'Content-Type': 'application/json' 
+      },
+      body: {
+        sub: 'a6d5c443-1f51-4783-ba1a-7686ffe3b54a',
         'https://purl.imsglobal.org/spec/lti/claim/deployment_id': '07940580-b309-415e-a37c-914d387c1150',
         'https://purl.imsglobal.org/spec/lti/claim/message_type': 'LtiResourceLinkRequest',
         'https://purl.imsglobal.org/spec/lti/claim/version': '1.3.0',
@@ -20,13 +22,30 @@ describe('Validate Launch Request', () => {
             'http://purl.imsglobal.org/vocab/lis/v2/membership#Mentor' ],
         'https://purl.imsglobal.org/spec/lti/claim/tool_platform': {
             'url': "https://platform.example.edu",
-          },    
-        'https://purl.imsglobal.org/spec/lti/claim/resource_link': 
-          { id: '200d101f-2c14-434a-a0f3-57c2a42369fd',
+        },    
+        'https://purl.imsglobal.org/spec/lti/claim/resource_link': {
+            id: '200d101f-2c14-434a-a0f3-57c2a42369fd',
             description: 'Assignment to introduce who you are',
-            title: 'Introduction Assignment' } },
-      json: true };
-    });
+            title: 'Introduction Assignment' 
+        }, 
+        'https://purl.imsglobal.org/spec/lti/claim/context': {
+            id: 'random-id-with-cha.dfokasdf',
+            label: 'react100',
+            title: 'React for Beginners',
+            type: [ 'http://purl.imsglobal.org/vocab/lis/v2/course#CourseOffering' ]
+        },
+        given_name: 'Joe',
+        family_name: 'Schmoo',
+        name: 'Joe Schmoo',
+        'https://purl.imsglobal.org/spec/lti-ags/claim/endpoint': {
+          scope: ['https://purl.imsglobal.org/spec/lti-ags/scope/score'],
+          'lineitems': 'https://www.myuniv.example.com/2344/lineitems/',
+          'lineitem': 'https://www.myuniv.example.com/2344/lineitems/1234/lineitem'
+        }
+      },
+      json: true 
+    };
+  });
 
   it('should reject non-POST requests', () => {
     expect(valid_launch_request(req)).to.have.lengthOf(0);
@@ -88,10 +107,61 @@ describe('Validate Launch Request', () => {
     expect(valid_launch_request(req)).to.have.lengthOf(0);
     req.body['https://purl.imsglobal.org/spec/lti/claim/roles'] = [];
     expect(valid_launch_request(req)).to.have.lengthOf(0);
-    req.body['https://purl.imsglobal.org/spec/lti/claim/roles'] = ['Ehttp://purl.imsglobal.org/vocab/lis/v2/institution/person#Student'];
+    req.body['https://purl.imsglobal.org/spec/lti/claim/roles'] = ['WRONGhttp://purl.imsglobal.org/vocab/lis/v2/institution/person#Student'];
     expect(valid_launch_request(req)).to.be.an('array').that.includes('Role invalid');
     delete req.body['https://purl.imsglobal.org/spec/lti/claim/roles'];
     expect(valid_launch_request(req)).to.be.an('array').that.includes('Role missing');
   });
 
+  it('should correctly validate Context if it is present', () => {
+    expect(valid_launch_request(req)).to.have.lengthOf(0);
+    delete req.body['https://purl.imsglobal.org/spec/lti/claim/context'].title;
+    expect(valid_launch_request(req)).to.have.lengthOf(0);
+    delete req.body['https://purl.imsglobal.org/spec/lti/claim/context'].label;
+    expect(valid_launch_request(req)).to.be.an('array').that.includes('Context invalid: does not contain label OR title');
+    req.body['https://purl.imsglobal.org/spec/lti/claim/context'].type = ['WRONGhttp://purl.imsglobal.org/vocab/lis/v2/course#CourseOffering'];
+    expect(valid_launch_request(req)).to.be.an('array').that.includes('Context invalid: type invalid');
+    req.body['https://purl.imsglobal.org/spec/lti/claim/context'].type = [];
+    expect(valid_launch_request(req)).to.be.an('array').that.includes('Context invalid: type invalid')
+    delete req.body['https://purl.imsglobal.org/spec/lti/claim/context'].type;
+    expect(valid_launch_request(req)).to.be.an('array').that.includes('Context type missing')
+  });
+  
+  it('should not give an error if Context is NOT present', () => {
+    delete req.body['https://purl.imsglobal.org/spec/lti/claim/context'];
+    expect(valid_launch_request(req)).to.be.an('array').that.is.empty;
+  });
+  
+  it('should check name information correctly', () => {
+    expect(valid_launch_request(req)).to.have.lengthOf(0);
+    req.body.name = 5;
+    expect(valid_launch_request(req)).to.be.an('array').that.includes('Name information invalid')
+  });
+
+  it('should not give an error if Name information is NOT present', () => {
+    delete req.body.name;
+    expect(valid_launch_request(req)).to.be.an('array').that.is.empty;
+    delete req.body.given_name;
+    expect(valid_launch_request(req)).to.be.an('array').that.is.empty;
+    delete req.body.family_name;
+    expect(valid_launch_request(req)).to.be.an('array').that.is.empty;
+  });
+
+  it('should correctly validate Endpoint if it is present', () => {
+    expect(valid_launch_request(req)).to.have.lengthOf(0);
+    delete req.body['https://purl.imsglobal.org/spec/lti-ags/claim/endpoint'].lineitem;
+    expect(valid_launch_request(req)).to.be.an('array').that.includes('Score setup invalid');
+    delete req.body['https://purl.imsglobal.org/spec/lti-ags/claim/endpoint'].lineitems;
+    expect(valid_launch_request(req)).to.be.an('array').that.includes('Score setup invalid');
+    req.body['https://purl.imsglobal.org/spec/lti-ags/claim/endpoint'].scope = [];
+    expect(valid_launch_request(req)).to.be.an('array').that.includes('Score setup invalid')
+    delete req.body['https://purl.imsglobal.org/spec/lti-ags/claim/endpoint'].scope;
+    expect(valid_launch_request(req)).to.be.an('array').that.includes('Score setup invalid')
+  });
+  
+  it('should not give an error if Endpoint is NOT present', () => {
+    delete req.body['https://purl.imsglobal.org/spec/lti/claim/endpoint'];
+    expect(valid_launch_request(req)).to.be.an('array').that.is.empty;
+  });
+  
 });
