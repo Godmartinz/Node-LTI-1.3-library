dafd6888644a95763710735e1dac5e3a2efce6e9
argenis0409 <argenis04_09@icloud.com>
2019-07-25 15:34:13 -0700

    created a student_score file to post the student score into the LMS

--- lti_lib/student_score.js
+++ lti_lib/student_score.js
@@ -2,22 +2,22 @@ const axios = require('axios');
 
 function grading_grade(grade, payload) {
 
-axios.post(payload["https://purl.imsglobal.org/spec/lti-ags/claim/endpoint"].lineitems, {
-    "userId": "3427",
-    "scoreGiven": grade, 
-    "scoreMaximum": 1, 
-    "comment": "Well done!", 
-    "timestamp": "2018-06-12T14:30:23.605Z", 
-    "activityProgress": "Completed",
-    "gradingProgress": "FullyGraded"
-})
-.then((res) => {
-  console.log(`statusCode: ${res.statusCode}`)
-  console.log(res)
-})
-.catch((error) => {
-  console.error(error)
-})
+    var headers = {
+        'Content-Type': 'application/json',
+    }
+    axios.post(payload["https://purl.imsglobal.org/spec/lti-ags/claim/endpoint"].lineitems, {
+        "userId": "3427",
+        "scoreGiven": grade,
+        "scoreMaximum": 1,
+        "comment": "Well done!",
+        "timestamp": "2018-06-12T14:30:23.605Z",
+        "activityProgress": "Completed",
+        "gradingProgress": "FullyGraded"
+
+    }, { headers: headers })
+        .catch((error) => {
+            console.error(error)
+        })
 
 }
 
