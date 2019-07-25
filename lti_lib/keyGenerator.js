08c9c1f06a6e1ff93b77827c5818ff223b15252a
Godfrey Martinez <godmartinz@gmail.com>
2019-07-25 15:30:46 -0700

    added KeyGenerator with proper encryption as specified by Oauth 2.0

--- lti_lib/keyGenerator.js
+++ lti_lib/keyGenerator.js
@@ -1,27 +1,55 @@
-const {generateKeyPair}= require('crypto');
+const { generateKeyPairSync } = require("crypto");
+const crypto = require("crypto");
 
 /*
-
+The Key Generator creates a pair of rsa keys to validate a session between the LMS and the Platform.
 */
-function keyGenerator(){
-generateKeyPair('rsa', {
-    modulusLength: 4096,
-    publicKeyEncoding: {
-        type: 'spki',
-        format: 'pem'
+
+function passPhrase() {
+  var phrase = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 255; i++) {
+    phrase += characters.charAt(Math.random() * characters.length);
+  }
+  
+  return phrase.toString();
+}
+//The signature and the verification needs to be updated with a proper consumerID or some other unique identifer
+function keyGenerator() {
+  var keys = [];
+
+  const { publicKey, privateKey } = generateKeyPairSync(
+    "rsa",
+    {
+      modulusLength: 4096,
+      publicKeyEncoding: {
+        type: "spki",
+        format: "pem"
+      },
+      privateKeyEncoding: {
+        type: "pkcs8",
+        format: "pem",
+        cipher: "aes-256-cbc",
+        passphrase: passPhrase()
+      }
     },
-    privateKeyEncoding: {
-        type: 'pkcs8',
-        format: 'pem',
-        cipher: 'aes-256-cbc-hmac-sha1',
-        passphrase: "hello moodle"
+    (err, publicKey, privateKey) => {
+      var sign = crypto.createSign("RSA-SHA256");
+      sign.update("ConsumerClientID");
+      const signature = sign.sign(privateKey, "base64");
+      console.info("signature: %s", signature);
+
+      const verify = crypto.createVerify("RSA-SHA256");
+      verify.update("ConsumerClientID");
+      const verified = verify.verify(publicKey, "base64");
+      console.info("is signature ok? %s", verified);
     }
-}, (err, publicKey, privateKey) => {
-    if((publicKey==null) || (privateKey ==null)){
-        console.log(err);
-    }
-    return publicKey+privateKey;
-});
+  );
+
+  keys.push(publicKey, privateKey);
+
+  return keys;
 }
 
-module.exports= {keyGenerator};
+module.exports = { keyGenerator };
