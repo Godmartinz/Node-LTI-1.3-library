0686f2c4ae6df71c0b7d435411c5c7a3d3bd09fb
argenis0409 <argenis04_09@icloud.com>
2019-07-26 09:53:26 -0700

    updated the student_score to check the scope

--- lti_lib/student_score.js
+++ lti_lib/student_score.js
@@ -2,23 +2,25 @@ const axios = require('axios');
 
 function grading_grade(grade, payload) {
 
-    var headers = {
-        'Content-Type': 'application/json',
-    }
-    axios.post(payload["https://purl.imsglobal.org/spec/lti-ags/claim/endpoint"].lineitems, {
-        "userId": "3427",
-        "scoreGiven": grade,
-        "scoreMaximum": 1,
-        "comment": "Well done!",
-        "timestamp": "2018-06-12T14:30:23.605Z",
-        "activityProgress": "Completed",
-        "gradingProgress": "FullyGraded"
+    if(payload["https://purl.imsglobal.org/spec/lti-ags/claim/endpoint"].scope.includes('https://purl.imsglobal.org/spec/lti-ags/scope/score') ) {
+    
+        var headers = {
+         'Content-Type': 'application/json',
+        }
+        axios.post(payload["https://purl.imsglobal.org/spec/lti-ags/scope/score"].lineitem + "/score", {
+            "userId":  payload.sub,
+            "scoreGiven": grade,
+            "scoreMaximum": 1,
+            "comment": "Well done!",
+            "timestamp": Date.now.toJSON(),
+            "activityProgress": "Completed",
+            "gradingProgress": "FullyGraded"
 
-    }, { headers: headers })
+        }, { headers: headers })
         .catch((error) => {
             console.error(error)
         })
-
+    } 
 }
 
 module.exports = { grading_grade };
