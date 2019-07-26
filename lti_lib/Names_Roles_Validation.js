4dd7b18ddf8be56328f853d7558ecbd5c676b90a
Godfrey Martinez <godmartinz@gmail.com>
2019-07-26 14:02:41 -0700

    saving progress

--- lti_lib/Names_Roles_Validation.js
+++ lti_lib/Names_Roles_Validation.js
@@ -1,10 +1,14 @@
 
 
 function getUsers() {
+    userType=`http%3A%2%2Fpurl.imsglobal.org%2Fvocab%2Flis%2Fv2%2Fmembership%23${role}`
+   
     fetch('ENDPOINT PROVIDED BY PLATFORM'), {
         method: 'get',
         headers: {
             Accept: 'application/vnd.ims.lti-nrps.v2.membershipcontainer+json'
         }
+    }.then( (users) =>{
+       return users.filter(user =>user.role==`${userType}`);
     }
-}
+
