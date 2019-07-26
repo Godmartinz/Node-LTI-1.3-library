a02b74689ac4306dd7bac53acc9152a3005c7737
Godfrey Martinez <godmartinz@gmail.com>
2019-07-26 10:12:52 -0700

    Names and Roles

---
+++ lti_lib/Names_Roles_Validation.js
@@ -0,0 +1,10 @@
+
+
+function getUsers() {
+    fetch('ENDPOINT PROVIDED BY PLATFORM'), {
+        method: 'get',
+        headers: {
+            Accept: 'application/vnd.ims.lti-nrps.v2.membershipcontainer+json'
+        }
+    }
+}
