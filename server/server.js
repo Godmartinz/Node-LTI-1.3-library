818f5de955c60c50320322b2f7bd2a713e766bac
Sherry Freitas <sherry.freitas@gmail.com>
2019-08-01 16:37:47 -0700

    server for passing on CircleCI

--- server/server.js
+++ server/server.js
@@ -43,12 +43,12 @@ app.set("view engine", "ejs");
 /*
 * Setup MongoDB to store Platform data
 */
-mongoose.connect(/*process.env.MONGODB_URI*/'mongodb://localhost:27017/TESTLTI', {
+mongoose.connect(process.env.MONGODB_URI/*'mongodb://localhost:27017/TESTLTI'*/, {
   useNewUrlParser: true, 
-  // auth: {
-  //   user: process.env.MONGO_USER,
-  //   password: process.env.MONGO_PASSWORD
-  // }
+  auth: {
+    user: process.env.MONGO_USER,
+    password: process.env.MONGO_PASSWORD
+  }
 },
   (err) => {
     if(err) {
@@ -140,13 +140,13 @@ app.post(`/project/grading`, (req, res) => {
   .then(grading => {
     if (!grading.error) {
       req.session.grade = grading.grade;
-      const redir = prep_send_score(req);
-      res.redirect(307, redir);
+      // const redir = prep_send_score(req);
+      // res.redirect(307, redir);
     }
-    // res.render("submit", {
-    //   payload: req.session.payload, 
-    //   formData: grading
-    // });
+    res.render("submit", {
+      payload: req.session.payload, 
+      formData: grading
+    });
   });
 });
 
